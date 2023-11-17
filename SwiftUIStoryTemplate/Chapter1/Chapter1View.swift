2a710bcf885ad8db7b30513b45309b7ea665994c
dezdoesit <dez.blair@icloud.com>
2023-11-17 13:41:21 -0500

    Chap1Fix

--- SwiftUIStoryTemplate/Chapter1/Chapter1View.swift
+++ SwiftUIStoryTemplate/Chapter1/Chapter1View.swift
@@ -2,10 +2,12 @@ import SwiftUI
 
 struct Chapter1View: View {
     var body: some View {
-        Text("Chapter 1")
+        NavigationStack {
+            PageOne()
+        }
     }
 }
 
 #Preview {
-    Chapter1View()
+    HubPage()
 }
